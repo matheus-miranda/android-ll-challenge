2c29132556b0054734860a3be42e1a5f174df52f
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2024-04-25 00:51:46 -0300

    add screen transitions

--- app/src/main/java/com/luizalabs/delivery/navigation/RootNavHost.kt
+++ app/src/main/java/com/luizalabs/delivery/navigation/RootNavHost.kt
@@ -5,12 +5,23 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.luizalabs.registration.navgraph.REGISTRATION_GRAPH_ROUTE
 import com.luizalabs.registration.navgraph.featureRegistrationGraph
+import com.luizalabs.ui.screenFadeIn
+import com.luizalabs.ui.screenFadeOut
+import com.luizalabs.ui.screenSlideIn
+import com.luizalabs.ui.screenSlideOut
 
 @Composable
 internal fun RootNavHost() {
     val rootNavController = rememberNavController()
 
-    NavHost(navController = rootNavController, startDestination = REGISTRATION_GRAPH_ROUTE) {
+    NavHost(
+        navController = rootNavController,
+        startDestination = REGISTRATION_GRAPH_ROUTE,
+        enterTransition = { screenSlideIn() },
+        exitTransition = { screenFadeOut() },
+        popEnterTransition = { screenFadeIn() },
+        popExitTransition = { screenSlideOut() },
+    ) {
         featureRegistrationGraph(
             navController = rootNavController,
         )
