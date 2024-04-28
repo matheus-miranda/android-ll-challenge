765e8163868a4922a31d653e66dba619ea88161e
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2024-04-28 13:06:49 -0300

    setup detekt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.detekt)
     kotlin("kapt")
 }
 
