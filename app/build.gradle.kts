3b0fa00b2974cd9be92b0c8189fe192231f9dae9
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2024-04-28 17:11:15 -0300

    proguard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
             isDebuggable = true
         }
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
