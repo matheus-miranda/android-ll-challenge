3d3dbdd5819581eb45804bfbebd2ad311cddbefd
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2024-05-31 00:06:02 -0300

    compose compiler plugin for kotlin 2.0

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -25,9 +26,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
